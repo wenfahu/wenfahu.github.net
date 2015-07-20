e8d517d8d4eaf88400f1de1f4f33e64973d2bfdc
Fahu Wen <wenfahu@outlook.com>
2015-07-20 16:17:24 +0800

    friction

--- works/final/js/script.js
+++ works/final/js/script.js
@@ -2,6 +2,11 @@ var x = 120;
 var y = 100;
 var dx = 2;
 var dy = 8;
+var velX = 0;
+var velY = 0;
+var speed = 10;
+var friction = 0.98;
+var keys = [];
 var ctx;
 var width;
 var height;
@@ -17,7 +22,7 @@ function init() {
     height = $("#canvas").height();
     paddlex = width / 2;
     paddley = height - paddleh;
-    intervalId = setInterval(draw, 20);
+    intervalId = setInterval(update, 20);
     return intervalId;
 }
 
@@ -44,54 +49,76 @@ var rightKeyDown = false;
 var upKeyDown = false;
 var downKeyDown = false;
 
-/*
-$(document).keydown(function(e) {
-    if(e.keyCode == 39) {
-        rightKeyDown = true;
-    } else if(e.keyCode = 37) {
-        leftKeyDown = true;
-    } else if(e.keyCode = 38){
-        upKeyDown = true;
-    } else if(e.keyCode = 40){
-        downKeyDown = true;
+function update(){
+    //up
+    if(keys[38]){
+        if(velY > -speed){
+            velY--;
+        }
     }
-});
-$(document).keyup(function(e) {
-    if(e.keyCode == 39) {
-        rightKeyDown = false;
-    } else if(e.keyCode == 37) {
-        leftKeyDown = false;
-    } else if(e.keyCode = 38){
-        upKeyDown = false;
-    } else if(e.keyCode = 40){
-        downKeyDown = false;
+    //down
+    if(keys[40]){
+        if(velY < speed){
+            velY++;
+        }
     }
-});
-*/
-function keyDown(e){
- switch(e.keyCode){
-     //left
-     case 37:
-     paddlex -= 40;
-     break;
-     //up
-     case 38:
-     paddley -= 20;
-     break;
-     //right
-     case 39:
-     paddlex += 40;
-     break;
-     //down
-     case 40:
-     paddley += 20;
-     break;
-     //default:
-     //console.log('unknown position');
- }
+    //right
+    if(keys[39]){
+        if(velX < speed){
+            velX++;
+        }
+    }
+    //left
+    if(keys[37]){
+        if(velX > -speed){
+            velX--;
+        }
+    }
+
+
+    velY *= friction;
+    paddley += velY;
+    velX *= friction;
+    paddlex += velX;
+
+    if( paddley >= 295){
+        paddley = 295;
+    } else if(paddley <= 5){
+        paddley = 5;
+    }
+
+    if( paddlex >= 245){
+        paddlex = 245;
+    } else if(paddlex <= 5){
+        paddlex = 5;
+    }
+
+    clear();
+    circle(x, y, 10);    
+    rect(paddlex, paddley, paddlew, paddleh);
+    if (x + dx > width || x + dx < 0) {
+        dx = -dx;
+    }
+    if (y + dy < 0) {
+        dy = -dy;
+    } else if (y + dy > paddley) {
+        if (x > paddlex && x < paddlex + paddlew) {
+            dy = -dy;
+        } else {
+            if(y + dy < height){
+
+            }else{
+                clearInterval(intervalId);
+            }
+        }
+    }
+    x += dx;
+    y += dy;
+
+    //setTimeout(update, 10);
 };
 
-
+/*
 function draw() {
     clear();
     circle(x, y, 10);    
@@ -115,6 +142,17 @@ function draw() {
     x += dx;
     y += dy;
 }
+*/
 
 init();
-window.addEventListener('keydown', keyDown, true);
+window.addEventListener('keydown', function(e) {
+    keys[e.keyCode] = true;
+    // body...
+    //update();
+});
+
+window.addEventListener('keyup', function(e) {
+    // body...
+    keys[e.keyCode] = false;
+    //update();
+});
